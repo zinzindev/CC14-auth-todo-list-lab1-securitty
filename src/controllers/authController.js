414252fc406b0182ae17abf64f49b5d1557825de
zinzindev <zinzin30@windowslive.com>
2023-08-15 12:33:00 +0700

    finish

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,20 @@
+const bcrypt = require('bcryptjs');
+
+const {User} = require('../models')
+
 exports.register = async (req, res, next) => {
 	try {
+		const { username, email, password, confirmPassword } = req.body;
+
+		if (password !== confirmPassword) {
+			return res.status(400).json({
+				message: 'password and confirm password did not match',
+			});
+		}
+		const hashedPassword = await bcrypt.hash(password, 10);
+		await User.create({ username, email, password: hashedPassword });
+
+		res.status(201).json({ message: 'user created' });
 	} catch (error) {
 		next(error);
 	}
@@ -7,7 +22,7 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
 	try {
- 	} catch (error) {
+	} catch (error) {
 		next(error);
 	}
 };
